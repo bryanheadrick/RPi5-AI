5449194b45dd8a91e49d7ab04bb9a259dd3bba22
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2024-12-28 02:28:18 -0800

    Update startOpenWebUI.sh

--- startOpenWebUI.sh
+++ startOpenWebUI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker run -d -p 3000:8080 -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main
-docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+sudo docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama

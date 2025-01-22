a9dff0c048f7980ac488086bd25e371f02979774
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2025-01-21 22:24:33 -0800

    Update startStableDiffusion.sh
    Changed profile to automatic1111 because comfy is not working and I haven't been able to figure it out yet.

--- startStableDiffusion.sh
+++ startStableDiffusion.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/AbdBarho/stable-diffusion-webui-docker
 cd stable-diffusion-webui-docker
 sudo docker compose --profile download up --build
-sudo docker compose --profile comfy-cpu up --build
+sudo docker compose --profile auto-cpu up --build

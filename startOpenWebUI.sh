dd829eed891c1123142233d9762a7c4e36ed322a
Bryan Headrick <support@catmanstudios.com>
2025-02-02 10:45:05 -0500

    update openwebui script to use cuda

--- startOpenWebUI.sh
+++ startOpenWebUI.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+sudo docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
